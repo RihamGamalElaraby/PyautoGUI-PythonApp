ffbf21225de287889d99e6b17b3ce809776af265
RehamElaraby <rehamelaraby2272@gmail.com>
2024-08-03 22:53:10 +0200

    update

--- pyautogui_test.py
+++ pyautogui_test.py
@@ -1,17 +1,37 @@
+"""
 import time
 
 import pyautogui
 
-# Get screen size
+pyautogui.click(21,751)
 screen_size = pyautogui.size()
 print(f"Screen size: {screen_size}")
+pyautogui.move(200,168)
+time.sleep(2)
+print(pyautogui.position())
+#Point(x=56, y=33)
+sleep(2)
+pyautogui.doubleClick(56 , 33)
+"""
+import time
 
-# Display mouse position for a short period
-print("Move your mouse to see its position (Press Ctrl+C to stop)...")
-try:
-    while True:
-        x, y = pyautogui.position()
-        print(f"Mouse position: (X: {x}, Y: {y})", end='\r')
-        time.sleep(0.1)
-except KeyboardInterrupt:
-    print("\nStopped displaying mouse position.")
+import pyautogui
+
+time.sleep(1)
+pyautogui.click(69, 750)
+path = 'C:\\Users\\mohamed\\AppData\\Local\\Programs\\Python\\Python311\\Scripts'
+pyautogui.write(path)
+time.sleep(2)
+pyautogui.press('enter')
+# 851,62cmd
+time.sleep(1.5)
+pyautogui.click(851, 62)
+pyautogui.write('cmd')
+time.sleep(1.5)
+pyautogui.press('enter')
+time.sleep(1.5)
+install = 'pip install pyautogui'
+pyautogui.write(install)
+
+
+ 
